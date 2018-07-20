9d4fc91fdc3886a972c1262a3f4c8aeda94ce9e8
MeyerFabian <fabian.meyer1337@web.de>
2018-07-20 05:45:02 +0200

    small change to do read style from file

--- clang-format-all.bat
+++ clang-format-all.bat
@@ -1,2 +1,2 @@
 # recursively do clang format on all cpp and hpp files in cmd bash
-for /f "usebackq delims=|" %%f in (`dir /s /b ^|findstr "\.cpp \.hpp"`) do call clang-format -i -style=Google %%f
+for /f "usebackq delims=|" %%f in (`dir /s /b ^|findstr "\.cpp \.hpp"`) do call clang-format -i -style=file %%f
