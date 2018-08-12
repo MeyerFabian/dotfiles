de9f9c434244019c20f82c5b4153963604b967b0
MeyerFabian <fabian.meyer1337@web.de>
2018-08-12 11:17:09 +0200

    added automated call

--- scripts/bat/ahkscripts_autostart.bat
+++ scripts/bat/ahkscripts_autostart.bat
@@ -1,3 +1,5 @@
 ::run as admin!
 mklink "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\StayHydrated.ahk" "%HOME%\scripts\ahk\StayHydrated.ahk"
 mklink "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\CapslockEsc.ahk" "%HOME%\scripts\ahk\CapslockEsc.ahk"
+start /B "" "%HOME%\scripts\ahk\CapslockEsc.ahk"
+start /B "" "%HOME%\scripts\ahk\StayHydrated.ahk"
